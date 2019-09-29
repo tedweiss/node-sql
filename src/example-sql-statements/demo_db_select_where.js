086b6e800cd18e9fbe2bcb6627a024a312a8dc37
Ted Weiss <theodore_weiss@outlook.com>
2019-09-28 20:54:11 -0400

    add demo select where

---
+++ src/example-sql-statements/demo_db_select_where.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+
+const login = require('../utils/login')
+const dbName = 'mydb'
+
+const con = mysql.createConnection({
+  host: login.host,
+  user: login.user,
+  password: login.password,
+  database: dbName
+})
+
+con.connect(err => {
+  if (err) throw err
+  con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", (err, result) => {
+    if (err) throw err
+    console.log(result)
+  })
+})
